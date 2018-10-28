80422ab5e0947cd5d88dba5f83c46491cc178470
AKHIL YENDLURI <akhilyendluri@AKHILs-MacBook-Pro.local>
2018-10-28 13:15:38 -0400

    Consumer completed

--- blogspot/src/main/java/com/cs1699/project/blogspot/config/ConsumerConfiguration.java
+++ blogspot/src/main/java/com/cs1699/project/blogspot/config/ConsumerConfiguration.java
@@ -1,53 +1,59 @@
-//package com.cs1699.project.blogspot.config;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import org.apache.kafka.clients.consumer.ConsumerConfig;
-//import org.apache.kafka.common.serialization.StringDeserializer;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.annotation.EnableKafka;
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-//import org.springframework.kafka.core.ConsumerFactory;
-//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-//import org.springframework.kafka.support.serializer.JsonDeserializer;
-//import org.springframework.kafka.support.serializer.JsonSerializer;
-//
-//import com.cs1699.project.blogspot.datasource.Blog;
-//
-//@EnableKafka
-//@Configuration
-//public class ConsumerConfiguration {
-//	
-//	@Value("${spring.kafka.bootstrapserver}")
-//	private String bootstrapServer;
-//	
-//	@Bean
-//	public Map<String, Object> consumerConfig() {
-//		
-//		Map<String, Object> props = new HashMap<>();
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "foo");
-//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//        return props;
-//		
-//	}
-////	
-////	public ConsumerFactory<String, Blog> consumerFactory() {
-////		return new DefaultKafkaConsumerFactory<>(consumerConfig(), new StringDeserializer(), 
-////				new JsonDeserializer<>(Blog.class));
-////	}
-////	
-////	@Bean
-////	public ConcurrentKafkaListenerContainerFactory<String, Blog> concurrentKafkaListenerContainerFactory() {
-////		ConcurrentKafkaListenerContainerFactory<String, Blog> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
-////		concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
-////		return concurrentKafkaListenerContainerFactory;
-////	}
-//	
-//}
+package com.cs1699.project.blogspot.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.cs1699.project.blogspot.datasource.Blog;
+
+@Configuration
+@EnableKafka
+public class ConsumerConfiguration {
+
+    /**
+     * List of Kafka servers that comes from the configuration file.
+     */
+	@Value("${spring.kafka.bootstrapserver}")
+    private String bootstrapServers;
+
+    /**
+     * @return Map of Kafka configuration options.
+     */
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "foo");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return props;
+    }
+    
+    @Bean
+    public ConsumerFactory<String, Blog> consumerFactory() {
+      return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
+          new JsonDeserializer<>(Blog.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Blog> kafkaListenerContainerFactory() {
+      ConcurrentKafkaListenerContainerFactory<String, Blog> factory =
+          new ConcurrentKafkaListenerContainerFactory<>();
+      factory.setConsumerFactory(consumerFactory());
+
+      return factory;
+    }
+}
--- blogspot/src/main/java/com/cs1699/project/blogspot/config/ConsumerService.java
+++ blogspot/src/main/java/com/cs1699/project/blogspot/config/ConsumerService.java
@@ -1,38 +1,38 @@
-//package com.cs1699.project.blogspot.config;
-//
-//import java.util.ArrayList;
-//import java.util.concurrent.CountDownLatch;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//import com.cs1699.project.blogspot.datasource.Blog;
-//
-//@Component
-//public class ConsumerService {
-//
-//	private static final Logger LOGGER = LoggerFactory.getLogger(ProducerService.class);
-//	
-//	private CountDownLatch latch = new CountDownLatch(1);
-//	
-//	private ArrayList<Blog> blogs = new ArrayList<>();
-//	
-//	public CountDownLatch getLatch() {
-//		return latch;
-//	}
-//	
-//	public ArrayList<Blog> getBlogs() {
-//		return blogs;
-//	}
-//	
-//	@KafkaListener(topics = "test")
-//	public void receive(Blog blog) {
-//		
-//		System.out.println(blog.toString());
-//		latch.countDown();
-//		blogs.add(blog);
-//	}
-//	
-//}
+package com.cs1699.project.blogspot.config;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.cs1699.project.blogspot.datasource.Blog;
+
+@Component
+public class ConsumerService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProducerService.class);
+	
+	private CountDownLatch latch = new CountDownLatch(1);
+	
+	private ArrayList<Blog> blogs = new ArrayList<>();
+	
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+	
+	public ArrayList<Blog> getBlogs() {
+		return blogs;
+	}
+	
+	@KafkaListener(topics = "test")
+	public void receive(Blog blog) {
+		
+		System.out.println(blog.toString());
+		latch.countDown();
+		blogs.add(blog);
+	}
+	
+}
